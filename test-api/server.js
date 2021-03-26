65d6ac90b7f779a8975ed36caf66a87a2da2ce13
sergio-nunez-meneses <snunezmeneses@gmail.com>
2021-03-26 14:44:57 +0100

    added base node development environment

---
+++ test-api/server.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express    = require('express');
+const app        = express();
+const port       = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.listen(port, () => {
+	console.log(`server is running on port ${port}.`);
+});
+
+app.get('/', (req, res) => {
+	res.json({message: 'connection successful'});
+});
