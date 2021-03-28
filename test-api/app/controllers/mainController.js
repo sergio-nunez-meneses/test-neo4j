11dbf55bcd435ae0169c680f5f895f54e62cce00
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-03-28 16:36:49 +0200

    minor changes in mainController.js file

--- test-api/app/controllers/mainController.js
+++ test-api/app/controllers/mainController.js
@@ -12,25 +12,27 @@ exports.getNodeLabelFromUrl = async function(req) {
 	return data;
 }
 
-exports.create = ash(async function(req, res, endpoint) {
-	console.log('function call:', req.body);
+exports.create = ash(async function(req, res) {
+	console.log('function called:', req.body); // just for debugging
 
+	// testing getNodeLabelFromUrl function
+	const url        = req.originalUrl.split('/');
+	const endpoint   = url[2].includes('?') ? url[2].split('?')[0] : url[2];
 	const label      = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
+
 	const session    = driver.session();
-
-	// const instance   = await mainRepository.createOne(label, req.body);
-
 	const result = await session.run(
 			`CREATE (a:${label} $properties) RETURN a`,
 			{properties: req.body},
 	);
+	// const instance   = await mainRepository.createOne(label, req.body);
 
 	const singleRecord = result.records[0];
 	const node         = singleRecord.get(0);
 
 	await session.close();
 
-	console.log(node);
+	console.log('result:', node); // just for debugging
 
 	res.json({message: 'node ' + node.properties.name + ' created successfully!'});
 });
